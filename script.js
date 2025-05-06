4f411cec1819004a86a5fef348fa81f486513c9c
crumbcrumb <116420859+crumbcrumb@users.noreply.github.com>
2025-05-05 21:20:57 -0500

    added if statement to second function and declared variables

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const billAmountTipElm = document.getElementById("bill-amount-tip");
 const tipPercentElm = document.getElementById("tip-p");
-const totalTipElm = document.getElementsById("total-tip-amount");
+const totalTipElm = document.getElementById("total-tip-amount");
 const tipBtn = document.getElementById("tip-btn");
 
 const billAmountSplitElm = document.getElementById("bill-amount-split");
@@ -8,21 +8,31 @@ const numOfPeopleElm = document.getElementById("total-people");
 const totalSplitElm = document.getElementById("total-split-amount");
 const splitBtn = document.getElementById("split-btn");
 
+// tipBtn.addEventListener("click", handleCalculateTip);
 
-tipBtn.addEventListener("click", handleCalculateTip)
+// function handleCalculateTip(event) {
+//   event.preventDefault();
+//   let input1 = billAmountTipElm;
+//   let input2 = tipPercentElm;
+//   if (!isNaN(billAmountTipElm) && !isNaN(tipPercentElm)) {
+//   }
+ 
+//   event.target.reset();
+// }
 
-function handleCalculateTip(event) {
-    event.preventDefault();
-// let tip = billAmountTipElm * tipPercentElm
-// totalTipElm.textContent = `${tip}`;
-    event.target.reset();
-}
-
-splitBtn.addEventListener("click", handleCalculateSplit)
+splitBtn.addEventListener("click", handleCalculateSplit);
 
 function handleCalculateSplit(event) {
-    event.preventDefault();
-// let split = billAmountSplitElm / numOfPeopleElm
-// totalSplitElm.textContent = `${split}`;
-    event.target.reset();
+  event.preventDefault();
+  let input1 = billAmountSplitElm.value;
+  let input2 = numOfPeopleElm.value;
+  if (!isNaN(billAmountSplitElm) && !isNaN(numOfPeopleElm)) {
+    totalSplitElm.textContent = input1 / input2;
+  } else {
+    totalSplitElm.textContent = "Please enter a numeric value";
+  }
+
+  // let split = billAmountSplitElm / numOfPeopleElm
+  // totalSplitElm.textContent = `${split}`;
+  event.target.reset();
 }
